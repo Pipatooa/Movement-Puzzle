d2f2536fac62d9f9b6876cdac92f3bfc4912dde8
Pipatooa <42383774+Pipatooa@users.noreply.github.com>
2020-03-05 21:51:22 +0000

    Updated level assets

---
+++ Movement Puzzle/Assets/Scripts/Scriptable/LevelAssets.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelAssets", menuName = "Level Assets")]
+public class LevelAssets : ScriptableObject
+{
+    public GameObject player;
+    
+    public GameObject tile;
+    public GameObject goal;
+}
